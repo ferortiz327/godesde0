c4bb5a174beb280e9b54eed2950020310f528e1a
unknown <ferortiz327@gmail.com>
2023-10-26 21:52:06 -0500

    primer commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ferortiz327/godesde0
+
+go 1.21.3
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ferortiz327/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
