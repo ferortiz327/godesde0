db2149ef77c8302b9abf4cf6ab5d81403d2e7778
unknown <ferortiz327@gmail.com>
2023-10-27 14:23:21 -0500

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/ferortiz327/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConversionTexto(1588)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
